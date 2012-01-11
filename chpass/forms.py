6a61d701724df944c3a4ee986389be362b6589c8
kedo <kedo@ocf.berkeley.edu>
2012-01-11 14:57:35 -0800

    saved ocf_accounts in request.session and changed order of ChpassForm fields

--- chpass/forms.py
+++ chpass/forms.py
@@ -3,6 +3,15 @@ from recaptcha.fields import ReCaptchaField
 from chpass.validators import validate_password
 
 class ChpassForm(forms.Form):
+    def __init__(self, ocf_accounts, *args, **kwargs):
+        super(ChpassForm, self).__init__(*args, **kwargs)
+
+        self.fields["ocf_account"] = forms.ChoiceField(choices=[(x,x) for x in ocf_accounts], label="OCF Account")
+        self.fields.keyOrder = [
+            "ocf_account",
+            "new_password",
+            "recaptcha"
+        ]
     
     new_password = forms.CharField(widget=forms.PasswordInput, label="New Password", validators=[validate_password])
-    recaptcha = ReCaptchaField()
+    recaptcha = ReCaptchaField(label="ReCaptcha")
--- panda.py
+++ panda.py
@@ -2,17 +2,26 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from chpass.forms import ChpassForm
+from ocf.utils import users_by_calnet_uid
+from calnet.decorators import login_required as calnet_required
 
+@calnet_required
 def index(request):
-	dump = {}
-        if request.method == "POST":
-            form = ChpassForm(request.POST)
-            if form.is_valid():
-                dump["form"] = "VALID"
-        else:
-            form = ChpassForm()
-            
-        return render_to_response("test.html", {
-        	"form": form,
-        	"dump": dump
-        }, context_instance=RequestContext(request))
+    if not "ocf_accounts" in request.session:
+        request.session["ocf_accounts"] = users_by_calnet_uid(request.session["calnet_uid"])
+    accounts = request.session["ocf_accounts"]
+
+    dump = {}
+    for key in request.session.keys():
+        dump[key] = request.session[key]
+
+    if request.method == "POST":
+        form = ChpassForm(accounts, request.POST)
+        if form.is_valid():
+            dump["form"] = "VALID"
+    else:
+        form = ChpassForm(accounts)
+    return render_to_response("test.html", {
+        "form": form,
+        "dump": dump
+    }, context_instance=RequestContext(request))
