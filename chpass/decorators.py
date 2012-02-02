d5c9fbf1d0ecc536effa0503fe6c84577ca81728
Kenny Do <kennydo@berkeley.edu>
2012-02-02 01:08:05 -0800

    fixed typo

--- chpass/decorators.py
+++ chpass/decorators.py
@@ -4,6 +4,6 @@ def https_required(function=None):
     def _decorator(request, *args, **kwargs):
         if not request.is_secure():
             return HttpResponsePermanentRedirect("https://%s/%s" % \
-                (request.get_host(), request.get_full_path())
+                (request.get_host(), request.get_full_path()))
         return function(request, *args, **kwargs)
     return _decorator
