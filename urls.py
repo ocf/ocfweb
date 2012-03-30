5ce4c2f05c02959a5b31cade63c3372300a82587
Kenny Do <kennydo@berkeley.edu>
2012-03-29 21:24:08 -0700

    Added url for request_account

--- urls.py
+++ urls.py
@@ -9,6 +9,7 @@ urlpatterns = patterns('',
     # Example:
     # (r'^account_tools/', include('account_tools.foo.urls')),
     (r'^/?$', lambda x: redirect("/account_tools/change_password")),
+    (r'^request_account/?$', 'approve.views.request_account'),
     (r'^change_password/?$', 'chpass.views.change_password'),
     (r'^calnet/login/?$', 'calnet.views.login'),
     (r'^calnet/logout/?$', 'calnet.views.logout'),
