5bd8b77b3ed96b543c0e57c135148c72d3098ddf
Kenny Do <kedo@ocf.berkeley.edu>
2012-04-26 20:45:10 -0700

    moved approve.py to utils.py b/c of name causing import errors

--- approve/views.py
+++ approve/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.conf import settings
 from approve.forms import ApproveForm
-from approve.approve import run_approve
+from approve.utils import run_approve
 from ocf.decorators import https_required
 from ocf.utils import users_by_calnet_uid
 from calnet.decorators import login_required as calnet_required
