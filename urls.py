c4e07627fbf71feafd46e3f03a4355d3669566de
Kenny Do <kedo@ocf.berkeley.edu>
2012-05-03 23:58:13 -0700

    removed default comments from url

--- urls.py
+++ urls.py
@@ -1,21 +1,10 @@
 from django.conf.urls.defaults import *
 from django.shortcuts import redirect
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
 urlpatterns = patterns('',
-    # Example:
-    # (r'^account_tools/', include('account_tools.foo.urls')),
     (r'^/?$', lambda x: redirect("/account_tools/change_password")),
     (r'^request_account/?$', 'approve.views.request_account'),
     (r'^change_password/?$', 'chpass.views.change_password'),
     (r'^calnet/login/?$', 'calnet.views.login'),
     (r'^calnet/logout/?$', 'calnet.views.logout'),
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
 )
