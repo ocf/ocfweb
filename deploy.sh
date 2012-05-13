ad117d1e9aa0955699c26f5feed3dcd103e22cda
Kenny Do <kennydo@berkeley.edu>
2012-05-13 03:07:41 -0700

    fixed deploy script putting public_html account_tools files in wrong place

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 python setup.py install --home=/usr/local/www/django_projects
 
-cp -f public_html/account_tools/{.htaccess,account_tools.fcgi} /mnt/http/localhost
+cp -f public_html/account_tools/{.htaccess,account_tools.fcgi} /mnt/http/localhost/account_tools
 
 cd /usr/local/www/django_projects/lib/python
 cp /root/account_tools_settings.py.secret account_tools/settings.py
