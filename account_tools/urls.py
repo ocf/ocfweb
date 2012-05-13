4a661d3d2b6ac73e4c07ddc633804cf75d90e1c7
Kenny Do <kennydo@berkeley.edu>
2012-05-13 16:29:00 -0700

    fixed path errors

--- account_tools/urls.py
+++ account_tools/urls.py
@@ -3,8 +3,8 @@ from django.shortcuts import redirect
 
 urlpatterns = patterns('',
     (r'^/?$', lambda x: redirect("/account_tools/change_password")),
-    (r'^request_account/?$', 'approve.views.request_account'),
-    (r'^change_password/?$', 'chpass.views.change_password'),
-    (r'^calnet/login/?$', 'calnet.views.login'),
-    (r'^calnet/logout/?$', 'calnet.views.logout'),
+    (r'^request_account/?$', 'account_tools.approve.views.request_account'),
+    (r'^change_password/?$', 'account_tools.chpass.views.change_password'),
+    (r'^calnet/login/?$', 'account_tools.calnet.views.login'),
+    (r'^calnet/logout/?$', 'account_tools.calnet.views.logout'),
 )
--- public_html/account_tools/account_tools.fcgi
+++ public_html/account_tools/account_tools.fcgi
@@ -1,14 +1,8 @@
 #!/usr/bin/env python
 import sys, os
+import account_tools
 
-projects_location = "/usr/local/www/django_projects/lib/python"
-project_location = os.path.join(projects_location, "account_tools")
-
-# Add a custom Python path.
-sys.path.insert(0, projects_location)
-sys.path.insert(1, project_location)
-# Switch to the directory of your project. (Optional.)
-os.chdir(project_location)
+sys.path.insert(0, account_tools.__path__[0])
 
 # Set the DJANGO_SETTINGS_MODULE environment variable.
 os.environ['DJANGO_SETTINGS_MODULE'] = "account_tools.settings"
