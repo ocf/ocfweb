99975ab2e382104a7a398569ac1f9a75d7db6d13
Kenny Do <kennydo@berkeley.edu>
2012-05-13 17:07:30 -0700

    put deploy.sh into setup.py install_lib command

--- setup.py
+++ setup.py
@@ -1,8 +1,35 @@
 from distutils.core import setup
+from distutils.command.install_lib import install_lib
+
+import shlex
+import shutil
+import subprocess
+import os
+
+SETTINGS_SECRET_FILE = "/root/account_tools_settings.py.secret"
+
+
+class my_install_lib(install_lib):
+    def run(self):
+        install_lib.run(self)
+        package_location = os.path.join(self.install_dir, "account_tools")
+
+        print "Copying account_tools settings file"
+        destination = os.path.join(package_location, "settings.py")
+        print "Copying to %s" % destination
+        shutil.copy(SETTINGS_SECRET_FILE, destination)
+
+        print "Changing owner to www:www"
+        chown_cmd = "chown -R www:www %s" % package_location
+        subprocess.call(shlex.split(chown_cmd))
+
+        print "Chmod go-rwx"
+        chmod_cmd = "chmod -R go-rwx %s" % package_location
+        subprocess.call(shlex.split(chmod_cmd))
 
 setup(
-    name = "account_tools",
-    packages = [
+    name="account_tools",
+    packages=[
         "account_tools",
         "account_tools.approve",
         "account_tools.calnet",
@@ -11,23 +38,26 @@ setup(
         "account_tools.ocf.validators",
         "account_tools.recaptcha"
     ],
-    package_data = {
+    package_data={
         'account_tools.approve': ['reserved_names.txt'],
         'account_tools': ['templates/*.html'],
     },
-    data_files = [
+    data_files=[
         ('/mnt/http/localhost/account_tools', [
             'public_html/account_tools/.htaccess',
             'public_html/account_tools/account_tools.fcgi'
         ])
     ],
-    version = "0.0.1",
-    description = "OCF account manage Django app",
-    author = "Kenny Do",
-    author_email = "kedo@ocf.berkeley.edu",
-    url = "http://secure.ocf.berkeley.edu/account_tools",
-    keywords = ["ocf", "account", "password"],
-    classifiers = [
+    cmdclass={
+        "install_lib": my_install_lib
+    },
+    version="0.0.1",
+    description="OCF account manage Django app",
+    author="Kenny Do",
+    author_email="kedo@ocf.berkeley.edu",
+    url="http://secure.ocf.berkeley.edu/account_tools",
+    keywords=["ocf", "account", "password"],
+    classifiers=[
         "Programming Language :: Python",
         "Development Status :: 4 - Beta",
         "Framework :: Django",
