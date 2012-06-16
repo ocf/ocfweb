45da5cf8b8f6f299b2941cc6cedc1acd7c89c292
Kenny Do <kedo@ocf.berkeley.edu>
2012-06-16 02:35:59 -0700

    added the commands to be run to the first value of the choices tuple

--- account_tools/cmds/forms.py
+++ account_tools/cmds/forms.py
@@ -12,11 +12,11 @@ class CommandForm(forms.Form):
         max_length=64)
 
     COMMAND_CHOICES = (
-        ("paper quota", "paper quota — how many pages you have printed this semester"),
-        ("disk quota", "disk quota — how much disk space you have used and how much you have left"),
-        ("spam-setup", "spam-setup — set up SpamAssassin spam filtering for your OCF email"),
-        ("makehttp", "makehttp — set up the web space for your OCF account"),
-        ("makemysql", "makemysql — reset your MySQL database password, or create a new MySQL database (copy down the password somewhere SECURE)"),
+        ("/opt/ocf/bin/paper", "paper quota — how many pages you have printed this semester"),
+        ("/usr/bin/quota -svQ", "disk quota — how much disk space you have used and how much you have left"),
+        ("/opt/ocf/bin/spam-setup", "spam-setup — set up SpamAssassin spam filtering for your OCF email"),
+        ("/opt/ocf/bin/makehttp", "makehttp — set up the web space for your OCF account"),
+        ("echo yes | /opt/ocf/bin/makemysql", "makemysql — reset your MySQL database password, or create a new MySQL database (copy down the password somewhere SECURE)"),
     )
 
     command_to_run = forms.ChoiceField(choices=COMMAND_CHOICES,
