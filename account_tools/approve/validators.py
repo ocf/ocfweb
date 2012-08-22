2ea215179fff0a14a5dfa70b25ebc3e794a0e168
Nader Morshed <morshed.nader@gmail.com>
2012-08-22 16:27:49 -0700

    Fixed extra spaces in a docstring (GRAAAHHH) Changed _host_exists() to catch only getaddrinfo exceptions

--- account_tools/approve/validators.py
+++ account_tools/approve/validators.py
@@ -1,24 +1,22 @@
-from socket import getaddrinfo
+from socket import getaddrinfo, gaierror
 from django.conf import settings
 from django.core.exceptions import ValidationError
 
 
 def _email_host(email_addr):
-    """ Returns the host in the email address argument """
+    """Returns the host in the email address argument"""
     if "@" in email_addr:
         return email_addr.rsplit("@", 1).pop()
     return None
 
 
 def _host_exists(host):
-    exists = False
     try:
         host_info = getaddrinfo(host, None)
-        if host_info:
-            exists = True
-    except:
-        pass
-    return exists
+    except gaierror:
+        return False
+    else:
+        return host_info
 
 
 def validate_email_host_exists(email_addr):
