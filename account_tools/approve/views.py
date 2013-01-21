7ceb7e1caec5d46592b231c29a89ed26d1aaf264
Nader Morshed <morshed@ocf.berkeley.edu>
2013-01-21 15:07:37 -0800

    Renamed approve.py to ocf_approve.py to fix an import error

--- account_tools/approve/views.py
+++ account_tools/approve/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.conf import settings
 from approve.forms import ApproveForm
-from approve.approve import approve_user, ApprovalError
+from approve.ocf_approve import approve_user, ApprovalError
 from ocf.decorators import https_required
 from ocf.utils import users_by_calnet_uid
 from calnet.decorators import login_required as calnet_required
