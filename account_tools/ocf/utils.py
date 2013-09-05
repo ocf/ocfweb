c1fbcaf29aa645b68c46041e618fdfe29687dca3
Dara Adib <daradib@ocf.berkeley.edu>
2013-09-04 23:42:43 -0700

    Fix capitalization of calnetUid attribute

--- account_tools/ocf/utils.py
+++ account_tools/ocf/utils.py
@@ -32,7 +32,7 @@ def users_by_calnet_uid(calnet_uid):
     l = ldap.initialize(settings.OCF_LDAP)
     l.simple_bind_s("", "")
 
-    search_filter = "(calNetuid=%s)" % calnet_uid
+    search_filter = "(calnetUid=%s)" % calnet_uid
     attrs = ["uid"]
 
     ldap_entries = l.search_st("ou=People,dc=OCF,dc=Berkeley,dc=EDU",
