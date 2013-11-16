b84a85e4fe05e961f8672564f1f89bd1f445546c
Chris Kuehl <chris@techxonline.net>
2013-11-16 14:31:01 -0800

    Add functions to get user attrs and account type.

--- account_tools/ocf/utils.py
+++ account_tools/ocf/utils.py
@@ -37,6 +37,35 @@ def users_by_calnet_uid(calnet_uid):
 
     return [entry[1]["uid"][0] for entry in ldap_entries]
 
+def user_attrs(user_account):
+    """Returns a dictionary of LDAP attributes for a given LDAP UID in
+    the form:
+        
+    {
+      'uid': ['somebody'],
+      'objectClass': ['ocfAccount', 'account', 'posixAccount'],
+      'loginShell': ['/bin/zsh']
+    }
+    
+    Returns None if no account exists with uid=user_account.
+    """
+
+    l = ldap.initialize(settings.OCF_LDAP)
+    l.simple_bind_s("", "")
+
+    search_filter = "(uid=%s)" % user_account
+
+    ldap_entries = l.search_st("ou=People,dc=OCF,dc=Berkeley,dc=EDU",
+                               ldap.SCOPE_SUBTREE, search_filter)
+
+    if len(ldap_entries) > 0:
+        return ldap_entries[0][1]
+
+def user_is_group(user_account):
+    """Returns True if an OCF user account exists and is a group account"""
+
+    attrs = user_attrs(user_account)
+    return attrs and "oslGid" in attrs
 
 def user_exists(user_account):
     """Returns True if an OCF user exists with specified account name"""
