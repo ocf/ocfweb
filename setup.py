d202cd462c5af93f148d89144abd4566e4e8957c
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2015-06-06 19:41:53 -0700

    Build Debian package with dh-virtualenv

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+VERSION = 1
+
+setup(
+    name='atool',
+    version=VERSION,
+    packages=find_packages(),
+    url='https://www.ocf.berkeley.edu/',
+    author='Open Computing Facility',
+    author_email='help@ocf.berkeley.edu',
+)
