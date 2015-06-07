dd45d81a9304e8754248d377c16bec6a91e71723
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2015-06-07 13:57:54 -0700

    Load settings from /etc/, generate secret in postinst

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     name='atool',
     version=VERSION,
     packages=find_packages(),
+    include_package_data=True,
     url='https://www.ocf.berkeley.edu/',
     author='Open Computing Facility',
     author_email='help@ocf.berkeley.edu',
