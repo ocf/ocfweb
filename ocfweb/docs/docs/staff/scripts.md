cb437f91119b82a9fb829a67954cf4a3eea73ea6
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2015-08-29 12:59:32 -0700

    Replace links to wiki.ocf, remove lab page

--- ocfweb/docs/docs/staff/scripts.md
+++ ocfweb/docs/docs/staff/scripts.md
@@ -1,6 +1,7 @@
 [[!meta title="Scripts"]]
 
-Create subpages under this category to document scripts/programs that are commonly used in the OCF.  To find documentation for a script, simply add its name to the end of this URL (e.g.: you can find information about the `pkusers` program at https://wiki.ocf.berkeley.edu/scripts/pkusers/).
+Create subpages under this category to document scripts/programs that are
+commonly used in the OCF.
 
 ## Scripts Index
 
--- ocfweb/urls.py
+++ ocfweb/urls.py
@@ -4,7 +4,6 @@ from ocfweb.docs.docs import doc
 from ocfweb.docs.docs import docs_index
 from ocfweb.main.favicon import favicon
 from ocfweb.main.home import home
-from ocfweb.main.lab import lab
 from ocfweb.main.staff_hours import staff_hours
 
 
@@ -12,7 +11,6 @@ urlpatterns = [
     url('^$', home, name='home'),
     url('^favicon.ico$', favicon, name='favicon'),
     url('^staff-hours$', staff_hours, name='staff-hours'),
-    url('^lab$', lab, name='lab'),
 
     url('^docs/$', docs_index, name='docs'),
     # this is carefully constructed to prevent extra slashes at the start/end
