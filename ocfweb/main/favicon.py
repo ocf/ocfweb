25934761bf81300a12d566d317805df8943323f7
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2015-09-19 13:08:45 -0700

    Move favicons into separate img directory

--- ocfweb/main/favicon.py
+++ ocfweb/main/favicon.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 
 def favicon(request):
     """favicon.ico must be served from the root for legacy reasons."""
-    with open(join(dirname(dirname(__file__)), 'static', 'img', 'favicon.ico'), 'rb') as f:
+    with open(join(dirname(dirname(__file__)), 'static', 'img', 'favicon', 'favicon.ico'), 'rb') as f:
         return HttpResponse(
             f.read(),
             content_type='image/x-icon',
