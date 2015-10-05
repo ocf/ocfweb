a235e43cc85d677b741834a7258204c732a29813
Matthew McAllister <matthew.mcallister@berkeley.edu>
2015-10-04 18:03:22 -0700

    Reformat lambdas split across lines

--- ocfweb/urls.py
+++ ocfweb/urls.py
@@ -20,8 +20,8 @@ urlpatterns = [
     url('^staff-hours$', staff_hours, name='staff-hours'),
 
     # hosting logos
-    url(r'^images/hosted-logos/(?:index\.shtml)?$', lambda _: redirect(
-        reverse('doc', args=('services/vhost/badges',)), permanent=True)),
+    url(r'^images/hosted-logos/(?:index\.shtml)?$',
+        lambda _: redirect(reverse('doc', args=('services/vhost/badges',)), permanent=True)),
     url('^images/hosted-logos/(.*)$', lambda _, image: redirect('hosting_logo', image, permanent=True)),
     url('^hosting-logos/(.*)$', hosting_logo, name='hosting_logo'),
 
@@ -30,22 +30,16 @@ urlpatterns = [
     url(r'^staff_hours(?:\.cgi)?$', lambda _: redirect(reverse('staff-hours'), permanent=True)),
     url(r'^staff-hours\.cgi$', lambda _: redirect(reverse('staff-hours'), permanent=True)),
     url(r'^OCF/(?:index\.shtml)?$', lambda _: redirect(reverse('doc', args=('about',)), permanent=True)),
-    url(r'^OCF/(?:past_)?officers.shtml$', lambda _:
-        redirect(reverse('doc', args=('about/officers',)),
-                 permanent=True
-                 )),
+    url(r'^OCF/(?:past_)?officers.shtml$',
+        lambda _: redirect(reverse('doc', args=('about/officers',)), permanent=True)),
     url(r'^OCF/staff/(?:index\.shtml)?$', lambda _: redirect(reverse('doc', args=('staff',)), permanent=True)),
-    url(r'^OCF/staff/where-now\.shtml$', lambda _:
-        redirect(reverse('doc', args=('about/formerstaff',)),
-                 permanent=True
-                 )),
+    url(r'^OCF/staff/where-now\.shtml$',
+        lambda _: redirect(reverse('doc', args=('about/formerstaff',)), permanent=True)),
     url(r'^OCF/policies(?:/|$)', lambda _: redirect(reverse('docs'), permanent=True)),
     url(r'^OCF/OCF_FAQ\.shtml$', lambda _: redirect(reverse('doc', args=('faq',)), permanent=True)),
     url(r'^OCF/officers_.*\.s?html$', lambda _: redirect(reverse('doc', args=('about/officers',)), permanent=True)),
-    url(r'^OCF/staff/how-to-join\.shtml$', lambda _: redirect(
-        'https://hello.ocf.berkeley.edu/',
-        permanent=True
-    )),
+    url(r'^OCF/staff/how-to-join\.shtml$',
+        lambda _: redirect('https://hello.ocf.berkeley.edu/', permanent=True)),
 
     url('^docs', include(docs)),
 ]
