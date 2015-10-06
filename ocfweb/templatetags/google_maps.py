5fa19ef1b0e7f716977c25318801b6cf62020769
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2015-10-06 15:56:59 -0700

    Add Google Maps back to home page

--- ocfweb/templatetags/google_maps.py
+++ ocfweb/templatetags/google_maps.py
@@ -1,3 +1,4 @@
+from urllib.parse import urlencode
 from uuid import uuid4
 
 from django import template
@@ -5,10 +6,37 @@ from django import template
 register = template.Library()
 
 
+# public api key
+OCF_API_KEY = 'AIzaSyC4st-AHFMr1O-59eaoT2AGjvX_zHtbdN8'
+
+OCF_LATITUDE = 37.869488
+OCF_LONGITUDE = -122.257522
+
+
 @register.inclusion_tag('partials/google-map.html')
-def google_map(width, height):
+def google_map(width, height, show_info=True):
     return {
         'width': width,
         'height': height,
+        'show_info': show_info,
         'id': uuid4(),
+        'ocf_api_key': OCF_API_KEY,
+        'ocf_latitude': OCF_LATITUDE,
+        'ocf_longitude': OCF_LONGITUDE,
+    }
+
+
+@register.inclusion_tag('partials/google-map-static.html')
+def google_map_static(width, height):
+    return {
+        'url': 'https://maps.googleapis.com/maps/api/staticmap?{}'.format(
+            urlencode({
+                'size': '{}x{}'.format(width, height),
+                'zoom': 17,
+                'center': '{},{}'.format(OCF_LATITUDE, OCF_LONGITUDE),
+                'markers': 'label:OCF|size:mid|{},{}'.format(OCF_LATITUDE, OCF_LONGITUDE),
+
+                'key': OCF_API_KEY,
+            })
+        ),
     }
