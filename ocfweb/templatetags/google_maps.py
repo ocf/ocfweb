4ee3abc0b9aa7e1107a6ae482a7e5169c7342ed5
Matthew McAllister <matthew.mcallister@berkeley.edu>
2015-11-14 01:18:21 -0800

    Replace references to old lab

--- ocfweb/templatetags/google_maps.py
+++ ocfweb/templatetags/google_maps.py
@@ -9,11 +9,8 @@ register = template.Library()
 # public api key
 OCF_API_KEY = 'AIzaSyC4st-AHFMr1O-59eaoT2AGjvX_zHtbdN8'
 
-OCF_LATITUDE = 37.869488
-OCF_LONGITUDE = -122.257522
-
-NEW_OCF_LATITUDE = 37.869179
-NEW_OCF_LONGITUDE = -122.259364
+OCF_LATITUDE = 37.869724
+OCF_LONGITUDE = -122.259715
 
 
 @register.inclusion_tag('partials/google-map.html')
@@ -26,20 +23,6 @@ def google_map(width, height, show_info=True):
         'ocf_api_key': OCF_API_KEY,
         'ocf_latitude': OCF_LATITUDE,
         'ocf_longitude': OCF_LONGITUDE,
-    }
-
-
-# TODO: remove this duplicated code and template
-@register.inclusion_tag('partials/new-google-map.html')
-def new_google_map(width, height, show_info=True):
-    return {
-        'width': width,
-        'height': height,
-        'show_info': show_info,
-        'id': uuid4(),
-        'ocf_api_key': OCF_API_KEY,
-        'ocf_latitude': NEW_OCF_LATITUDE,
-        'ocf_longitude': NEW_OCF_LONGITUDE,
     }
 
 
