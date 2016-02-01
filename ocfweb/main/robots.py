6b128f50a917e8c31a202b6e2a7f18c075de9616
Jason Perrin <jasonvperrin@gmail.com>
2016-02-01 15:41:16 -0800

    Disallow all pages in robots.txt when in development

--- ocfweb/main/robots.py
+++ ocfweb/main/robots.py
@@ -1,15 +1,25 @@
 from textwrap import dedent
 
+from django.conf import settings
 from django.http import HttpResponse
 
 
 def robots_dot_txt(request):
     """Serve /robots.txt file."""
-    return HttpResponse(
-        dedent("""\
+    if settings.DEBUG:
+        resp = """\
+            User-Agent: *
+            Disallow: /
+        """
+    else:
+        resp = """\
             User-Agent: *
             Disallow: /login/calnet/
             Disallow: /test/
-        """),
+        """
+
+    return HttpResponse(
+        dedent(resp),
         content_type='text/plain',
     )
+
