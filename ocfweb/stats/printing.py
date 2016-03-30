1b39730b888f93dfbf2a0145144f3bdb5de36dbd
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2016-03-29 17:46:09 -0700

    stats/printing: use enforcer instead of PyKota for histogram

--- ocfweb/stats/printing.py
+++ ocfweb/stats/printing.py
@@ -1,24 +1,11 @@
-import pymysql
 from django.http import HttpResponse
 from django.shortcuts import render
 from matplotlib.figure import Figure
+from ocflib.printing.quota import get_connection
+from ocflib.printing.quota import SEMESTERLY_QUOTA
 
 from ocfweb.caching import periodic
 from ocfweb.stats.plot import plot_to_image_bytes
-
-
-# TODO: improve/move this after PyKota is gone
-CURRENT_SEMESTER_QUOTA = 100
-
-
-def pykota_connection(user='ocfpykota_user', password='not_a_secret'):
-    return pymysql.connect(
-        host='mysql.ocf.berkeley.edu',
-        user=user,
-        password=password,
-        db='ocfpykota',
-        cursorclass=pymysql.cursors.DictCursor,
-    )
 
 
 def stats_printing(request):
@@ -40,19 +27,18 @@ def semester_histogram(request):
 
 @periodic(300)
 def _semester_histogram():
-    with pykota_connection() as c:
+    with get_connection() as c:
         c.execute(
-            'SELECT GREATEST(0, `balance`) AS `balance` FROM `users` WHERE `balance` < %s',
-            (CURRENT_SEMESTER_QUOTA,),
+            'SELECT `user`, `semester` FROM `printed` WHERE `semester` > 0',
         )
-        users = [r['balance'] for r in c]
+        users = [SEMESTERLY_QUOTA - int(r['semester']) for r in c]
 
     fig = Figure(figsize=(10, 5))
     ax = fig.add_subplot(1, 1, 1)
     ax.locator_params(nbins=20)
-    ax.hist(users, bins=20)
+    ax.hist(users, bins=list(range(0, 105, 5)))
     ax.grid(True)
-    ax.set_xlim(CURRENT_SEMESTER_QUOTA, 0)
+    ax.set_xlim(SEMESTERLY_QUOTA, 0)
     ax.set_ylabel('Number of users')
     ax.set_xlabel('Remaining balance')
     ax.set_title('Remaining balances this semester')
