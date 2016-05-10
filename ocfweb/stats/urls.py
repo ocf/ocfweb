ded84de6c9f4e216085194fdbb3b2e477329ea16
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2016-05-09 17:10:11 -0700

    Split out stats urlpatterns

---
+++ ocfweb/stats/urls.py
@@ -0,0 +1,28 @@
+from django.conf.urls import url
+
+from ocfweb.stats.accounts import accounts_created_each_day_graph
+from ocfweb.stats.accounts import cumulative_accounts_graph
+from ocfweb.stats.accounts import cumulative_group_accounts_graph
+from ocfweb.stats.accounts import stats_accounts
+from ocfweb.stats.daily_graph import daily_graph_image
+from ocfweb.stats.printing import semester_histogram
+from ocfweb.stats.printing import stats_printing
+from ocfweb.stats.sessions import sessions_image
+from ocfweb.stats.summary import summary
+
+
+urlpatterns = [
+    url(r'^$', summary, name='stats'),
+    url(r'^daily-graph/graph$', daily_graph_image, name='daily_graph_image'),
+    url(r'^sessions/graph$', sessions_image, name='sessions_image'),
+    url(r'^printing/$', stats_printing, name='stats_printing'),
+    url(r'^accounts/$', stats_accounts, name='stats_accounts'),
+    url(r'^accounts/cumulative/graph$', cumulative_accounts_graph, name='cumulative_accounts_graph'),
+    url(
+        r'^accounts/cumulative-groups/graph$',
+        cumulative_group_accounts_graph,
+        name='cumulative_group_accounts_graph',
+    ),
+    url(r'^accounts/daily/graph$', accounts_created_each_day_graph, name='accounts_created_each_day_graph'),
+    url(r'^printing/semester-histogram/graph$', semester_histogram, name='semester_histogram'),
+]
--- ocfweb/urls.py
+++ ocfweb/urls.py
@@ -15,15 +15,7 @@ from ocfweb.main.home import home
 from ocfweb.main.hosting_logos import hosting_logo
 from ocfweb.main.robots import robots_dot_txt
 from ocfweb.main.staff_hours import staff_hours
-from ocfweb.stats.accounts import accounts_created_each_day_graph
-from ocfweb.stats.accounts import cumulative_accounts_graph
-from ocfweb.stats.accounts import cumulative_group_accounts_graph
-from ocfweb.stats.accounts import stats_accounts
-from ocfweb.stats.daily_graph import daily_graph_image
-from ocfweb.stats.printing import semester_histogram
-from ocfweb.stats.printing import stats_printing
-from ocfweb.stats.sessions import sessions_image
-from ocfweb.stats.summary import summary
+from ocfweb.stats.urls import urlpatterns as stats
 from ocfweb.test.periodic import test_list_periodic_functions
 from ocfweb.test.session import test_session
 
@@ -37,10 +29,14 @@ urlpatterns = [
     url(r'^$', home, name='home'),
     url(r'^robots\.txt$', robots_dot_txt, name='robots.txt'),
     url(r'^favicon.ico$', favicon, name='favicon'),
+
     url(r'^staff-hours$', staff_hours, name='staff-hours'),
 
-    # announcements
+    url(r'^account/', include(account)),
     url(r'^announcements/', include(announcements)),
+    url(r'^docs/', include(docs)),
+    url(r'^login/', include(login)),
+    url(r'^stats/', include(stats)),
 
     # about pages
     url(r'^about/staff$', about_staff, name='about-staff'),
@@ -51,21 +47,6 @@ urlpatterns = [
         lambda _: redirect(reverse('doc', args=('services/vhost/badges',)), permanent=True)),
     url(r'^images/hosted-logos/(.*)$', lambda _, image: redirect('hosting-logo', image, permanent=True)),
     url(r'^hosting-logos/(.*)$', hosting_logo, name='hosting-logo'),
-
-    # stats
-    url(r'^stats/$', summary, name='stats'),
-    url(r'^stats/daily-graph/graph$', daily_graph_image, name='daily_graph_image'),
-    url(r'^stats/sessions/graph$', sessions_image, name='sessions_image'),
-    url(r'^stats/printing/$', stats_printing, name='stats_printing'),
-    url(r'^stats/accounts/$', stats_accounts, name='stats_accounts'),
-    url(r'^stats/accounts/cumulative/graph$', cumulative_accounts_graph, name='cumulative_accounts_graph'),
-    url(
-        r'^stats/accounts/cumulative-groups/graph$',
-        cumulative_group_accounts_graph,
-        name='cumulative_group_accounts_graph',
-    ),
-    url(r'^stats/accounts/daily/graph$', accounts_created_each_day_graph, name='accounts_created_each_day_graph'),
-    url(r'^stats/printing/semester-histogram/graph$', semester_histogram, name='semester_histogram'),
 
     # legacy redirects
     url(r'^index\.s?html$', lambda _: redirect(reverse('home'), permanent=True)),
@@ -82,10 +63,4 @@ urlpatterns = [
     url(r'^OCF/officers_.*\.s?html$', lambda _: redirect(reverse('doc', args=('about/officers',)), permanent=True)),
     url(r'^OCF/staff/how-to-join\.shtml$', lambda _: redirect(reverse('about-staff'), permanent=True)),
     url(r'^mlk$', lambda _: redirect(reverse('doc', args=('services/lab',)), permanent=True)),
-
-    url(r'^docs/', include(docs)),
-
-    url(r'^account/', include(account)),
-
-    url(r'^login/', include(login)),
 ]
