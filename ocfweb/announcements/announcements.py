4300433d5cea6cef3797691779246dc104efd342
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2016-05-12 01:18:11 -0700

    Add announcements page and EFF announcement

---
+++ ocfweb/announcements/announcements.py
@@ -0,0 +1,110 @@
+from collections import namedtuple
+from datetime import date
+from datetime import datetime
+from datetime import time
+
+from cached_property import cached_property
+from django.contrib.staticfiles.templatetags.staticfiles import static
+from django.core.urlresolvers import reverse
+from django.shortcuts import render
+from django.utils import timezone
+
+from ocfweb.component.blog import get_blog_posts
+
+announcements = ()
+
+
+class Announcement(namedtuple('Announcement', ('title', 'date', 'path', 'render'))):
+
+    @cached_property
+    def link(self):
+        return reverse(self.route_name)
+
+    @cached_property
+    def route_name(self):
+        return '{}-announcement'.format(self.path)
+
+    @cached_property
+    def datetime(self):
+        """This is pretty silly, but Django humanize needs a datetime."""
+        return timezone.make_aware(
+            datetime.combine(self.date, time()),
+            timezone.get_default_timezone(),
+        )
+
+
+def announcement(title, date, path):
+    def wrapper(fn):
+        global announcements
+        announcements += (Announcement(
+            title=title,
+            date=date,
+            path=path,
+            render=lambda request: fn(title, request),
+        ),)
+        return fn
+    return wrapper
+
+
+def index(request):
+    return render(
+        request,
+        'announcements/index.html',
+        {
+            'title': 'News from the staff team',
+
+            'announcements': announcements,
+            'blog_posts': get_blog_posts()[:10],
+        },
+    )
+
+
+@announcement(
+    "OCF affirms support for EFF's Electronic Frontier Alliance",
+    date(2016, 5, 12),
+    'ocf-eff-alliance',
+)
+def renaming_announcement(title, request):
+    return render(
+        request,
+        'announcements/2016-05-12-ocf-eff-alliance.html',
+        {
+            'title': title,
+        },
+    )
+
+
+@announcement(
+    'Unveiling a new name for the Open Computing Facility',
+    date(2016, 4, 1),
+    'printing',
+)
+def renaming_announcement(title, request):
+    return render(
+        request,
+        'announcements/2016-04-01-renaming.html',
+        {
+            'title': title,
+            'og_title': title,
+            'og_image': request.build_absolute_uri(static('img/announcements/thanks-for-flying-ofc-og.png')),
+            'description': (
+                'The Board of Directors has voted to rename the Open Computing Facility '
+                'to the Open Facility for Computing (OFC).'
+            ),
+        },
+    )
+
+
+@announcement(
+    'Changes to printing policies',
+    date(2016, 2, 9),
+    'renaming',
+)
+def printing_announcement(title, request):
+    return render(
+        request,
+        'announcements/2016-02-09-printing.html',
+        {
+            'title': title,
+        },
+    )
--- ocfweb/announcements/urls.py
+++ ocfweb/announcements/urls.py
@@ -1,35 +1,16 @@
 from django.conf.urls import url
-from django.contrib.staticfiles.templatetags.staticfiles import static
-from django.shortcuts import render
 
-
-def announcement_2016_04_01_renaming(request):
-    return render(
-        request,
-        'announcements/2016-04-01-renaming.html',
-        {
-            'title': 'Unveiling a new name for the Open Computing Facility',
-            'og_title': 'Unveiling a new name for the Open Computing Facility',
-            'og_image': request.build_absolute_uri(static('img/announcements/thanks-for-flying-ofc-og.png')),
-            'description': (
-                'The Board of Directors has voted to rename the Open Computing Facility '
-                'to the Open Facility for Computing (OFC).'
-            ),
-        },
-    )
-
-
-def announcement_2016_02_09_printing(request):
-    return render(
-        request,
-        'announcements/2016-02-09-printing.html',
-        {
-            'title': 'Changes to printing policies',
-        },
-    )
+from ocfweb.announcements.announcements import announcements
+from ocfweb.announcements.announcements import index
 
 
 urlpatterns = [
-    url(r'^2016-02-09/printing$', announcement_2016_02_09_printing, name='printing-announcement'),
-    url(r'^2016-04-01/renaming-ocf$', announcement_2016_04_01_renaming, name='renaming-announcement'),
+    url(r'^$', index, name='announcements'),
+] + [
+    url(
+        '^{}/{}$'.format(announcement.date.isoformat(), announcement.path),
+        announcement.render,
+        name=announcement.route_name,
+    )
+    for announcement in announcements
 ]
