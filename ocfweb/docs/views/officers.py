c361ec6645a7420424b8b0904ec8145ff29627e4
Jason Perrin <jvperrin@ocf.berkeley.edu>
2016-08-21 15:32:24 -0700

    Add summer officers

--- ocfweb/docs/views/officers.py
+++ ocfweb/docs/views/officers.py
@@ -108,6 +108,7 @@ BOD_TERMS = [
     Term('Summer 2015', gms=['nickimp'], sms=['ckuehl']),
     Term('Fall 2015', gms=['nickimp'], sms=['ckuehl']),
     Term('Spring 2016', gms=['nickimp'], sms=['jvperrin', 'mattmcal']),
+    Term('Summer 2016', gms=['nickimp'], sms=['jvperrin', 'mattmcal']),
 ]
 
 
