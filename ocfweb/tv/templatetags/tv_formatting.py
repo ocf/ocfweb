0e8ad95cabc93c33c2ef5ec0be4549df32021ea6
Abizer Lokhandwala <abizer@ocf.berkeley.edu>
2017-02-03 05:35:16 -0800

    add css, tv-formatting templatetags, templates, etc

---
+++ ocfweb/tv/templatetags/tv_formatting.py
@@ -0,0 +1,14 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def tv_lab_hours(hour):
+    if hour:
+        if hour.open.minute != 0 or hour.close.minute != 0:
+            fmt = '{:%-I:%M%P}-{:%-I%M%P}'
+        else:
+            fmt = '{:%-I%P}-{:%-I%P}'
+
+        return fmt.format(hour.open, hour.close)
