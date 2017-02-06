662b61f3cc3b9b3de1481b7d74d9904885c69203
Abizer Lokhandwala <abizer@ocf.berkeley.edu>
2017-02-06 00:33:36 -0800

    rename base, improve css

--- ocfweb/tv/templatetags/tv_formatting.py
+++ ocfweb/tv/templatetags/tv_formatting.py
@@ -7,8 +7,8 @@ register = template.Library()
 def tv_lab_hours(hour):
     if hour:
         if hour.open.minute != 0 or hour.close.minute != 0:
-            fmt = '{:%-I:%M%P}-{:%-I%M%P}'
+            fmt = '{:%-I:%M%P}–{:%-I%M%P}'
         else:
-            fmt = '{:%-I%P}-{:%-I%P}'
+            fmt = '{:%-I%P}–{:%-I%P}'
 
         return fmt.format(hour.open, hour.close)
