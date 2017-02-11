19a83c9f0a1f57effc55f62bc2dfdc403b1f986f
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2017-02-11 12:54:26 -0800

    Upgrade to celery 4, use stunnel instead of spiped

--- ocfweb/component/celery.py
+++ ocfweb/component/celery.py
@@ -4,6 +4,8 @@ This module is responsible for instantiating the Celery tasks used for account
 creation using our special credentials. Other modules should import from here,
 rather than from ocflib directly.
 """
+import ssl
+
 from celery import Celery
 from django.conf import settings
 from ocflib.account.submission import get_tasks as real_get_tasks
@@ -12,6 +14,21 @@ celery_app = Celery(
     broker=settings.CELERY_BROKER,
     backend=settings.CELERY_BACKEND,
 )
+# TODO: use ssl verification
+celery_app.conf.broker_use_ssl = {
+    'ssl_cert_reqs': ssl.CERT_NONE,
+}
+# `redis_backend_use_ssl` is an OCF patch which was proposed upstream:
+# https://github.com/celery/celery/pull/3831
+celery_app.conf.redis_backend_use_ssl = {
+    'ssl_cert_reqs': ssl.CERT_NONE,
+}
+
+# TODO: stop using pickle
+celery_app.conf.task_serializer = 'pickle'
+celery_app.conf.result_serializer = 'pickle'
+celery_app.conf.accept_content = {'pickle'}
+
 _tasks = real_get_tasks(celery_app)
 
 create_account = _tasks.create_account
