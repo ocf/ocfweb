b1d5d33468169c0a838aa245c6a9f46b00b5b503
Abizer Lokhandwala <abizer@ocf.berkeley.edu>
2017-09-01 18:16:42 -0700

    hours: fix for display

--- ocfweb/api/hours.py
+++ ocfweb/api/hours.py
@@ -51,6 +51,14 @@ def _get_hours():
     return all_shifts
 
 
+def _merge_shifts(first, second, staffer=True):
+    return Hour(
+        open=min(first.open, second.open),
+        close=max(first.close, second.close),
+        staffer=first.staffer if staffer else None,
+    )
+
+
 def _combine_shifts(shifts):
     """combine a list of shifts into a list of Hour()s.
 
@@ -71,41 +79,71 @@ def _combine_shifts(shifts):
 
     combined_shifts = []
 
-    initial = raw_shifts.pop(0)
+    shift = raw_shifts.pop(0)
     for next_shift in raw_shifts:
-        if (initial.close in next_shift or next_shift.close in initial) and \
-                initial.staffer == next_shift.staffer:
-            initial = Hour(
-                open=min(initial.open, next_shift.open),
-                close=max(initial.close, next_shift.close),
-                staffer=initial.staffer,
-            )
+        if (shift.close in next_shift or next_shift.close in shift) and \
+                shift.staffer == next_shift.staffer:
+            shift = _merge_shifts(shift, next_shift)
         else:
-            combined_shifts.append(initial)
-            initial = next_shift
+            combined_shifts.append(next_shift)
+            shift = next_shift
 
     # tail case where staffer condition doesn't trip on list end
-    combined_shifts.append(initial)
+    combined_shifts.append(shift)
 
     return combined_shifts
 
 
-@periodic(5 * 60)
 def _generate_regular_hours():
     raw_hours = _get_hours()
     return {day: _combine_shifts(shifts) for day, shifts in raw_hours.items()}
 
 
+@periodic(5 * 60)
+def display_hours():
+    """merge shifts even further for display.
+
+    ocflib previously provided a list of hours separated when there
+    were discontinuous shifts. This hours implementation separates
+    hours when different opstaff are on shift as well for future big
+    brother tracking purposes, but this breaks applications that depend on
+    a concise list of hours for display. This is a fix that presents hours
+    like the old implementation until the code can be refactored as part
+    of the overall big brother project, i.e., in ocflib.
+    """
+
+    regular_hours = _generate_regular_hours()
+
+    display_hours = {}
+
+    for day, hours in regular_hours.items():
+        merged_shifts = []
+
+        hour = hours.pop(0)
+        for next_hour in hours:
+            if hour.close in next_hour or next_hour.close in hour:
+                hour = _merge_shifts(hour, next_hour, False)
+            else:
+                merged_shifts.append(hour)
+                hour = next_hour
+
+        merged_shifts.append(hour)
+
+        display_hours[day] = merged_shifts
+
+    return display_hours
+
+
 def get_hours_all(request):
     return JsonResponse(
-        _generate_regular_hours(),
+        display_hours(),
         json_dumps_params={'default': lambda x: x.isoformat()},
     )
 
 
 def get_hours_today(request):
     return JsonResponse(
-        _generate_regular_hours()[date.today().weekday()],
+        display_hours()[date.today().weekday()],
         json_dumps_params={'default': lambda x: x.isoformat()},
         safe=False,
     )
