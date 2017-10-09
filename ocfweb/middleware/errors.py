c2d3c7c948eff975c9d24c02ae9e1ad7163dfa55
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2017-10-08 17:44:25 -0700

    Sanitize error messages a bit

--- ocfweb/middleware/errors.py
+++ ocfweb/middleware/errors.py
@@ -1,3 +1,4 @@
+import re
 from pprint import pformat
 from textwrap import dedent
 from traceback import format_exc
@@ -7,6 +8,29 @@ from django.http.response import Http404
 from ocflib.misc.mail import send_problem_report
 
 from ocfweb.component.errors import ResponseException
+
+
+SENSITIVE_WSGI_CONTEXT = frozenset((
+    'HTTP_COOKIE',
+    'CSRF_COOKIE',
+))
+
+
+def sanitize(msg):
+    """Attempt to sanitize out known-bad patterns."""
+    # Remove any dictionary references with "encrypted_password", e.g. limes like:
+    #   {'some_key': ..., 'encrypted_password': b'asdf', 'some_other_key': ...}
+    msg = re.sub(r"('encrypted_password': b?').+?('(?:,|}))", r'\1<REDACTED>\2', msg)
+    return msg
+
+
+def sanitize_wsgi_context(headers):
+    """Attempt to sanitize out known-bad WSGI context keys."""
+    headers = dict(headers)
+    for key in SENSITIVE_WSGI_CONTEXT:
+        if key in headers:
+            headers[key] = '<REDACTED>'
+    return headers
 
 
 class OcflibErrorMiddleware:
@@ -22,6 +46,9 @@ class OcflibErrorMiddleware:
         if isinstance(exception, Http404):
             # we don't care about reporting 404 errors
             return
+
+        traceback = sanitize(format_exc())
+        headers = sanitize_wsgi_context(request.META)
 
         try:
             send_problem_report(dedent(
@@ -44,13 +71,13 @@ class OcflibErrorMiddleware:
                 {session}
                 """
             ).format(
-                traceback=format_exc(),
+                traceback=traceback,
                 request=request,
                 host=request.get_host(),
                 path=request.get_full_path(),
                 is_secure=request.is_secure(),
                 session=pformat(dict(request.session)),
-                headers=pformat(request.META),
+                headers=pformat(headers),
             ))
         except Exception as ex:
             print(ex)  # just in case it errors again here
