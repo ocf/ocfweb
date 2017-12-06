fef644f8bbf8ed0dda0bae93c7f061cab8891884
Tony Liu <trliu@ocf.berkeley.edu>
2017-12-06 11:35:47 -0800

    Merge document.ready calls into single call

--- ocfweb/static/js/pages/account/register.js
+++ ocfweb/static/js/pages/account/register.js
@@ -1,46 +1,28 @@
-// Quick validation of username field
-$(document).ready(function() {
-    var finTypingCountdown = 250; // 250 milliseconds
-    var typingTimer;
-    var $input = $('#id_ocf_login_name');
-
-    // On keyup, start countdown
-    $input.keyup(function() {
-        clearTimeout(typingTimer);
-        typingTimer = setTimeout(validate_username, finTypingCountdown);
-    });
-
-    // On keydown, clear countdown
-    $input.keydown(function() {
-        clearTimeout(typingTimer);
-    });
-
-    function validate_username() {
-        $.ajax({
-             type: 'GET',
-             url: 'validate',
-             data: {'username': $input.val(),
-                    'real_name': $('#real-name').text()},
-             success: function(data) {
-                 var $username_feedback = $('#username-feedback')
-                 if(data.is_valid) {
-                    $input.parent().removeClass('has-error')
-                                   .addClass('has-success');
-                    $username_feedback.removeClass('alert-danger')
-                                      .addClass('alert-success');
-                 } else {
-                    $input.parent().removeClass('has-success')
-                                   .addClass('has-error');
-                    $username_feedback.removeClass('alert-sucess')
-                                      .addClass('alert-danger');
-                 }
-                 $username_feedback.show().text(data.msg);
+function validate_username() {
+    var $username_field = $('#id_ocf_login_name'),
+        $username_feedback = $('#username-feedback');
+    $.ajax({
+         type: 'GET',
+         url: 'validate',
+         data: {'username': $username_field.val(),
+                'real_name': $('#real-name').text()},
+         success: function(data) {
+             if(data.is_valid) {
+                $username_field.parent().removeClass('has-error')
+                               .addClass('has-success');
+                $username_feedback.removeClass('alert-danger')
+                                  .addClass('alert-success');
+             } else {
+                $username_field.parent().removeClass('has-success')
+                               .addClass('has-error');
+                $username_feedback.removeClass('alert-sucess')
+                                  .addClass('alert-danger');
              }
-         });
-     }
-});
+             $username_feedback.show().text(data.msg);
+         }
+     });
+ }
 
-// Recommend usernames
 function recommend() {
     $.ajax({
          type: 'GET',
@@ -63,6 +45,25 @@ function recommend() {
          }
      });
 }
+
+
 $(document).ready(function() {
+    // Quick validation of username field
+    var finTypingCountdown = 250; // 250 milliseconds
+    var typingTimer;
+    var $input = $('#id_ocf_login_name');
+
+    // On keyup, start countdown
+    $input.keyup(function() {
+        clearTimeout(typingTimer);
+        typingTimer = setTimeout(validate_username, finTypingCountdown);
+    });
+
+    // On keydown, clear countdown
+    $input.keydown(function() {
+        clearTimeout(typingTimer);
+    });
+
+    // Load in recommendations
     recommend();
 });
