11c6f8830336f816fe08c51060888cd9ce2abfd0
Tony Liu <trliu@ocf.berkeley.edu>
2017-12-06 10:12:53 -0800

    Consolidate quick_username_validation and recommend_username into register.js

--- ocfweb/static/js/pages/account/register.js
+++ ocfweb/static/js/pages/account/register.js
@@ -1,3 +1,4 @@
+// Quick validation of username field
 $(document).ready(function() {
     var finTypingCountdown = 250; // 250 milliseconds
     var typingTimer;
@@ -37,4 +38,31 @@ $(document).ready(function() {
              }
          });
      }
+});
+
+// Recommend usernames
+function recommend() {
+    $.ajax({
+         type: 'GET',
+         url: 'recommend',
+         data: {'real_name': $('#real-name').text()},
+         success: function(data) {
+             $('#recommendations').empty();
+             var recommendations = data['recommendations'];
+             for (var i in recommendations) {
+                 var recommendation = recommendations[i];
+                 $('#recommendations').append(
+                     $('<button>', {
+                         type: 'button',
+                         class: 'list-group-item list-group-item-action list-group-item-success recommendation',
+                         onclick: '$("#id_ocf_login_name").val("' + recommendation + '").trigger("keyup");',
+                         text: recommendation,
+                     })
+                 );
+             }
+         }
+     });
+}
+$(document).ready(function() {
+    recommend();
 });
