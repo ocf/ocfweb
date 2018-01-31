148aac3fc077db87fa204367c95dc880b593011d
Ethan Smith <ethan@ethanhs.me>
2018-01-30 17:29:41 -0800

    Add password reset link

--- ocfweb/login/urls.py
+++ ocfweb/login/urls.py
@@ -1,5 +1,6 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
 
+from ocfweb.account.urls import urlpatterns as account
 from ocfweb.login.calnet import login as calnet_login
 from ocfweb.login.calnet import logout as calnet_logout
 from ocfweb.login.ocf import login
@@ -7,6 +8,8 @@ from ocfweb.login.ocf import logout
 
 
 urlpatterns = [
+    url(r'^account/', include(account)),
+
     url(r'^login/$', login, name='login'),
     url(r'^logout/$', logout, name='logout'),
 
