bd1d7896e966e8e9fab594c4a657dd61ccdde7c7
Vaibhav Jayaraman <vaibhavj@ocf.berkeley.edu>
2018-02-01 14:39:05 -0800

    made changes to staff_hours.html font size for different days

--- ocfweb/main/staff_hours.py
+++ ocfweb/main/staff_hours.py
@@ -1,9 +1,6 @@
 import time
-from datetime import date
-from datetime import timedelta
 
 from django.shortcuts import render
-from ocflib.lab.hours import Day
 from ocflib.lab.staff_hours import get_staff_hours as real_get_staff_hours
 
 from ocfweb.caching import periodic
@@ -12,31 +9,9 @@ from ocfweb.component.lab_status import get_lab_status
 
 @periodic(60, ttl=86400)
 def get_staff_hours():
+    print('IN OCFWEBOCFWEB:')
+    print(real_get_staff_hours())
     return real_get_staff_hours()
-
-
-def rotate_list(lst, shift):
-    return lst[shift:] + lst[:shift]
-
-
-def sort_days_of_week(Days):
-    for index in range(len(Days)):
-        if Days[index].weekday == 'Monday':
-            return rotate_list(Days, index)
-
-
-def weekend(day):
-    return day.weekday == 'Sunday' or day.weekday == 'Saturday'
-
-
-def remove_weekends(Days):
-    index = 0
-    while index < len(Days):
-        if weekend(Days[index]):
-            Days.pop(index)
-        else:
-            index += 1
-    return Days
 
 
 def staff_hours(request):
@@ -48,9 +23,5 @@ def staff_hours(request):
             'staff_hours': get_staff_hours(),
             'today': time.strftime('%A'),
             'lab_status': get_lab_status(),
-            'days_of_the_week': sort_days_of_week(remove_weekends([
-                Day.from_date(date.today() + timedelta(days=i))
-                for i in range(7)
-            ])),
         },
     )
