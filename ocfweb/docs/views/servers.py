2aee2c799a7f444da45a42260311e37a8b82d55f
Abizer Lokhandwala <abizer@ocf.berkeley.edu>
2018-04-27 17:21:09 -0700

    fix build: crisis is dead (long live deceit)

--- ocfweb/docs/views/servers.py
+++ ocfweb/docs/views/servers.py
@@ -61,11 +61,6 @@ class Host(namedtuple('Host', ['hostname', 'type', 'description', 'children'])):
 def get_hosts():
     return [
         Host.from_ldap(
-            hostname='crisis',
-            type='hypervisor',
-        ),
-
-        Host.from_ldap(
             hostname='hal',
             type='hypervisor',
             children=[
