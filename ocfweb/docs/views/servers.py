2d9a4057fc426104e201de08baf41da0531abb1d
Ziyao Zhang <ziyaointl@gmail.com>
2018-10-25 20:51:00 -0700

    Modify hypervisor PQL

--- ocfweb/docs/views/servers.py
+++ ocfweb/docs/views/servers.py
@@ -61,7 +61,7 @@ def is_hidden(host):
 
 
 PQL_GET_CHILDREN = "facts { name = 'vms' }"
-PQL_IS_VIRT_FALSE = "facts { name = 'is_virtual' and value = false }"
+PQL_IS_HYPERVISOR = 'resources[certname] { type = "Class" and title = "Ocf_kvm" }'
 
 
 def query_puppet(query):
@@ -69,15 +69,14 @@ def query_puppet(query):
     Accepts a PQL query, returns a dictionary with hostname as keys and
     'value' in the query output as values
     """
-    if query == PQL_IS_VIRT_FALSE:
-        output = '[ { "name": "is_virtual", "environment": "production", "certname": "firewhirl.ocf.berkeley.edu", "value": false }, { "name": "is_virtual", "environment": "production", "certname": "outbreak.ocf.berkeley.edu", "value": false }, { "name": "is_virtual", "environment": "production", "certname": "avalanche.ocf.berkeley.edu", "value": false }, { "name": "is_virtual", "environment": "abizer_aux", "certname": "dataloss.ocf.berkeley.edu", "value": false }, { "name": "is_virtual", "environment": "production", "certname": "bigbang.ocf.berkeley.edu", "value": false }, { "name": "is_virtual", "environment": "production", "certname": "blackout.ocf.berkeley.edu", "value": false }, { "name": "is_virtual", "environment": "production", "certname": "wildfire.ocf.berkeley.edu", "value": false }, { "name": "is_virtual", "environment": "production", "certname": "famine.ocf.berkeley.edu", "value": false }, { "name": "is_virtual", "environment": "production", "certname": "meteorstorm.ocf.berkeley.edu", "value": false }, { "name": "is_virtual", "environment": "production", "certname": "sinkhole.ocf.berkeley.edu", "value": false }, { "name": "is_virtual", "environment": "abizer", "certname": "hal.ocf.berkeley.edu", "value": false }, { "name": "is_virtual", "environment": "production", "certname": "invasion.ocf.berkeley.edu", "value": false }, { "name": "is_virtual", "environment": "abizer_aux", "certname": "jaws.ocf.berkeley.edu", "value": false }, { "name": "is_virtual", "environment": "production", "certname": "destruction.ocf.berkeley.edu", "value": false }, { "name": "is_virtual", "environment": "bzh", "certname": "corruption.ocf.berkeley.edu", "value": false }, { "name": "is_virtual", "environment": "production", "certname": "tornado.ocf.berkeley.edu", "value": false }, { "name": "is_virtual", "environment": "production", "certname": "cyanide.ocf.berkeley.edu", "value": false }, { "name": "is_virtual", "environment": "production", "certname": "plague.ocf.berkeley.edu", "value": false }, { "name": "is_virtual", "environment": "gleeb", "certname": "drought.ocf.berkeley.edu", "value": false }, { "name": "is_virtual", "environment": "production", "certname": "blizzard.ocf.berkeley.edu", "value": false }, { "name": "is_virtual", "environment": "production", "certname": "acid.ocf.berkeley.edu", "value": false }, { "name": "is_virtual", "environment": "production", "certname": "venom.ocf.berkeley.edu", "value": false }, { "name": "is_virtual", "environment": "production", "certname": "hurricane.ocf.berkeley.edu", "value": false }, { "name": "is_virtual", "environment": "production", "certname": "heatwave.ocf.berkeley.edu", "value": false }, { "name": "is_virtual", "environment": "production", "certname": "chaos.ocf.berkeley.edu", "value": false }, { "name": "is_virtual", "environment": "production", "certname": "volcano.ocf.berkeley.edu", "value": false }, { "name": "is_virtual", "environment": "production", "certname": "blight.ocf.berkeley.edu", "value": false }, { "name": "is_virtual", "environment": "production", "certname": "riptide.ocf.berkeley.edu", "value": false }, { "name": "is_virtual", "environment": "production", "certname": "hailstorm.ocf.berkeley.edu", "value": false }, { "name": "is_virtual", "environment": "production", "certname": "pandemic.ocf.berkeley.edu", "value": false }, { "name": "is_virtual", "environment": "production", "certname": "asteroid.ocf.berkeley.edu", "value": false }, { "name": "is_virtual", "environment": "production", "certname": "headcrash.ocf.berkeley.edu", "value": false }, { "name": "is_virtual", "environment": "production", "certname": "arsenic.ocf.berkeley.edu", "value": false }, { "name": "is_virtual", "environment": "production", "certname": "eruption.ocf.berkeley.edu", "value": false }, { "name": "is_virtual", "environment": "cooperc", "certname": "madcow.ocf.berkeley.edu", "value": false }, { "name": "is_virtual", "environment": "production", "certname": "cyclone.ocf.berkeley.edu", "value": false }, { "name": "is_virtual", "environment": "production", "certname": "surge.ocf.berkeley.edu", "value": false } ]'  # noqa: E501
-        output = eval(output.replace('false', 'False'))
+    if query == PQL_IS_HYPERVISOR:
+        output = eval('[ { "certname": "jaws.ocf.berkeley.edu" }, { "certname": "dev-hal.ocf.berkeley.edu" }, { "certname": "hal.ocf.berkeley.edu" }, { "certname": "riptide.ocf.berkeley.edu" }, { "certname": "pandemic.ocf.berkeley.edu" } ]')  # noqa: E501
     if query == PQL_GET_CHILDREN:
         output = eval('[ { "name": "vms", "environment": "abizer_aux", "value": [], "certname": "jaws.ocf.berkeley.edu" }, { "name": "vms", "environment": "production", "value": [ "fallingrocks", "cataclysm", "dev-flood", "dev-maelstrom", "dev-pestilence", "dev-tsunami", "dev-werewolves", "dev-whiteout", "doom", "fraud", "hozer-62", "limniceruption", "lowgpa", "miasma" ], "certname": "hal.ocf.berkeley.edu" }, { "name": "vms", "environment": "production", "value": [], "certname": "mudslide.ocf.berkeley.edu" }, { "name": "vms", "environment": "production", "value": [ "pox", "alamo", "apocalypse", "blackrain", "fallout", "falsevacuum", "fireball", "fukushima", "gnats", "leprosy", "malaria", "nuke", "oilspill", "panic", "pileup", "riot", "sarin", "shipwreck", "virus", "vortex", "war", "zerg", "walpurgisnacht", "mudslide", "pompeii", "locusts", "fire", "rapture", "smallpox", "aliens", "cloudburst", "coldwave", "coma", "dev-firestorm", "emp", "meltdown", "meteorite", "old-vampires", "quasar", "ragnarok", "revolution", "sauron", "skynet", "PQLest" ], "certname": "pandemic.ocf.berkeley.edu" }, { "name": "vms", "environment": "production", "value": [ "nyx", "dementors", "democracy", "whiteout", "reaper", "segfault", "monsoon", "fraud", "gridlock", "lethe", "anthrax", "supernova", "firestorm", "pestilence", "flood", "maelstrom", "thunder", "death", "tsunami", "lightning", "vampires", "werewolves", "biohazard", "cataclysm", "dev-dementors", "dev-flood", "dev-whiteout", "doom", "hozer-69", "hozer-70", "hozer-71", "hozer-73", "hozer-74", "limniceruption", "matrix", "miasma", "zombies" ], "certname": "riptide.ocf.berkeley.edu" }, { "name": "vms", "environment": "production", "value": [], "certname": "gnats.ocf.berkeley.edu" }, { "name": "vms", "environment": "production", "value": [], "certname": "smallpox.ocf.berkeley.edu" } ]')  # noqa: E501
     result = {}
     for d in output:
         hostname = d['certname'].split('.')[0]
-        result[hostname] = d['value']
+        result[hostname] = d.get('value')
     return result
 
 
@@ -101,12 +100,12 @@ def get_hosts():
     misc = create_hosts(hosts_by_filter('(type=printer)'))
     hypervisors = {}
 
-    physical_hosts = query_puppet(PQL_IS_VIRT_FALSE)
+    hypervisor_hostnames = query_puppet(PQL_IS_HYPERVISOR)
     all_children = query_puppet(PQL_GET_CHILDREN)
 
     # Add children to hypervisors
     for h in list(servers.values()):
-        if h.hostname in physical_hosts:
+        if h.hostname in hypervisor_hostnames:
             # Populate a list of children
             children = []
             for child_hostname in all_children.get(h.hostname, []):
@@ -131,10 +130,7 @@ def get_hosts():
         target_type_dict[hostname] = Host.from_ldap(hostname, type=host_type)
 
     change_host_type('overheat', 'raspi', servers, misc)
-    change_host_type('tornado', 'nuc', hypervisors, misc)
-    change_host_type('ransomware', 'hypervisor', servers, hypervisors)
-    change_host_type('dataloss', 'server', hypervisors, servers)
-    change_host_type('corruption', 'server', hypervisors, servers)
+    change_host_type('tornado', 'nuc', servers, misc)
     misc['blackhole'] = Host(
         'blackhole', 'network',
         'Arista 7050SX Switch.', [],
