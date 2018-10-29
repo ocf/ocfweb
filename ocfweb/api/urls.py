84acc930892dbf39dc6110af49a78d58dacdc5c5
NotRyan <ryanchan989@gmail.com>
2018-10-28 20:25:58 -0700

    add lab/staff page

--- ocfweb/api/urls.py
+++ ocfweb/api/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('hours/today', hours.get_hours_today, name='hours_today'),
     path('lab/desktops', lab.desktop_usage, name='desktop_usage'),
     path('session/log', session_tracking.log_session, name='log_session'),
+    path('lab/staff', lab.visible_staff, name='visible_staff'),
 ]
