a97f3ef4d0af63216b7df276a3ae69a13e64705c
George Bao Lee <gleeb@ocf.berkeley.edu>
2018-10-29 17:16:49 -0700

    Add lab survey page

--- ocfweb/urls.py
+++ ocfweb/urls.py
@@ -4,8 +4,7 @@ from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.urls import reverse
 
-from ocfweb.about.lab import lab_open_source
-from ocfweb.about.lab import lab_vote
+from ocfweb.about.lab import *
 from ocfweb.about.staff import about_staff
 from ocfweb.account.urls import urlpatterns as account
 from ocfweb.announcements.urls import urlpatterns as announcements
@@ -48,6 +47,7 @@ urlpatterns = [
     url(r'^about/staff$', about_staff, name='about-staff'),
     url(r'^about/lab/open-source$', lab_open_source, name='lab-open-source'),
     url(r'^about/lab/vote$', lab_vote, name='lab-vote'),
+    url(r'^about/lab/survey$', lab_survey, name='lab-survey'),
 
     # tv endpoints
     url(r'^tv/', include(tv)),
