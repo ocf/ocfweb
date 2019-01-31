fde86da4f35dd9be4d8c59b0321a2aaf8b1f6c95
Ziyao Zhang <ziyaointl@gmail.com>
2019-01-31 15:58:23 -0800

    Auto-update ocf.io/servers (#406)
    * Implement get_children(host_name)
    
    * Implement create_hosts(lst)
    
    * Replace old return statement
    
    * Add children to hypervisors
    
    * Handle special cases
    
    * Break up long lines
    
    * Fix build errors
    
    * Mark puppet query string as test-only
    
    Co-Authored-By: ziyaointl <ziyaointl@gmail.com>
    
    * Update switch type
    
    * Improve readability
    
    * Rewrite puppet query function
    
    * Update hypervisor assignment
    
    * Update special case handling
    
    * Rename PQL queries
    
    * Move create_hosts to global
    
    * Modify hypervisor PQL
    
    * Misc improvements
    
    * Update get children PQL query
    
    * Query puppet
    
    * Fix not enough values to unpack error
    
    * Modify caching frequency
    
    * Change cert directory
    
    * Use os.path.join when concatenating paths
    
    * Handle NXDOMAIN exceptions
    
    * Split query_puppet
    
    * Simplify special case handling
    
    * Sort hosts
    
    * Improve style
    
    * Update cert directory
    
    * Add __lt__ and __key to Host
    
    * Improve servers.py readability
    
    * Add error handling in query_puppet()
    
    * Refactor format_query_output(item)
    
    * Refactor ldap_to_host(item)
    
    * Improve docstrings
    
    * Refactor Host.__key(self)
    
    * Stylistic improvements
    
    * Refactor get_hosts()
    
    * Performance tweak
    
    * Improve style
    
    * Cache results forever

--- ocfweb/docs/views/servers.py
+++ ocfweb/docs/views/servers.py
@@ -1,21 +1,22 @@
+import os
 from collections import namedtuple
 
 import dns.resolver
+import requests
 from cached_property import cached_property
 from django.shortcuts import render
 from ocflib.infra.hosts import hosts_by_filter
-from ocflib.lab.stats import list_desktops
 
-from ocfweb.caching import periodic
+from ocfweb.caching import cache
+
+PUPPETDB_URL = 'https://puppetdb:8081/pdb/query/v4'
+PUPPET_CERT_DIR = '/etc/ocfweb/puppet-certs'
 
 
 class Host(namedtuple('Host', ['hostname', 'type', 'description', 'children'])):
-
-    # TODO: don't hard-code host types or children
-
     @classmethod
     def from_ldap(cls, hostname, type='vm', children=()):
-        host, = hosts_by_filter('(cn={})'.format(hostname))
+        host = hosts_by_filter('(cn={})'.format(hostname))
         if 'description' in host:
             description, = host['description']
         else:
@@ -29,13 +30,16 @@ class Host(namedtuple('Host', ['hostname', 'type', 'description', 'children'])):
 
     @cached_property
     def ipv4(self):
-        return str(dns.resolver.query(self.hostname, 'A')[0])
+        try:
+            return str(dns.resolver.query(self.hostname, 'A')[0])
+        except dns.resolver.NXDOMAIN:
+            return 'No IPv4 Address'
 
     @cached_property
     def ipv6(self):
         try:
             return str(dns.resolver.query(self.hostname, 'AAAA')[0])
-        except dns.resolver.NoAnswer:
+        except (dns.resolver.NoAnswer, dns.resolver.NXDOMAIN):
             return 'No IPv6 address'
 
     @cached_property
@@ -56,96 +60,98 @@ class Host(namedtuple('Host', ['hostname', 'type', 'description', 'children'])):
     def has_munin(self):
         return self.type in ('hypervisor', 'vm', 'server', 'desktop')
 
+    def __key(self):
+        """Key function used for comparison."""
+        ranking = {
+            'hypervisor': 1,
+            'server': 2,
+            'desktop': float('inf'),
+        }
+        default = 3
+        return (ranking.get(self.type, default), self.type, self.hostname)
 
-@periodic(120)
+    def __lt__(self, other_host):
+        return self.__key() < other_host.__key()
+
+
+def is_hidden(host):
+    return host['cn'][0].startswith('hozer-') or host['cn'][0].startswith('dev-')
+
+
+PQL_GET_VMS = "facts { name = 'vms' }"
+PQL_IS_HYPERVISOR = 'resources[certname] { type = "Class" and title = "Ocf_kvm" }'
+
+
+def query_puppet(query):
+    """Accepts a PQL query, returns a parsed json result."""
+    r = requests.get(
+        PUPPETDB_URL,
+        cert=(
+            os.path.join(PUPPET_CERT_DIR, 'puppet-cert.pem'),
+            os.path.join(PUPPET_CERT_DIR, 'puppet-private.pem'),
+        ),
+        verify=os.path.join(PUPPET_CERT_DIR, 'puppet-ca.pem'),
+        params={'query': query},
+    )
+    return r.json() if r.status_code == 200 else None
+
+
+def format_query_output(item):
+    """Converts an item of a puppet query to tuple(hostname, query_value)."""
+    return item['certname'].split('.')[0], item.get('value')
+
+
+def ldap_to_host(item):
+    """Accepts an ldap output item, returns tuple(hostname, host_object)."""
+    description = item.get('description', [''])[0]
+    hostname = item['cn'][0]
+    return hostname, Host(hostname, item['type'], description, ())
+
+
+@cache()
 def get_hosts():
-    return [
-        Host.from_ldap(
-            hostname='hal',
+    ldap_output = hosts_by_filter('(|(type=server)(type=desktop)(type=printer))')
+    servers = dict(ldap_to_host(item) for item in ldap_output if not is_hidden(item))
+
+    hypervisors_hostnames = dict(format_query_output(item) for item in query_puppet(PQL_IS_HYPERVISOR))
+    all_children = dict(format_query_output(item) for item in query_puppet(PQL_GET_VMS))
+
+    hostnames_seen = set()
+    servers_to_display = []
+    # Add children to hypervisors
+    for hypervisor_hostname in hypervisors_hostnames:
+        children = []
+        for child_hostname in all_children.get(hypervisor_hostname, []):
+            child = servers.get(child_hostname)
+            if child:
+                children.append(child._replace(type='vm'))
+                hostnames_seen.add(child.hostname)
+        description = servers[hypervisor_hostname].description if hypervisor_hostname in servers else None
+        servers_to_display.append(Host(
+            hostname=hypervisor_hostname,
             type='hypervisor',
-            children=[
-                Host.from_ldap(hostname)
-                for hostname in (
-                    'fallingrocks',
-                )
-            ],
+            description=description,
+            children=children,
+        ))
+        hostnames_seen.add(hypervisor_hostname)
+
+    # Handle special cases
+    for host in servers.values():
+        if host.hostname not in hostnames_seen:
+            servers_to_display.append(host)
+
+    servers_to_display.extend([
+        Host(
+            hostname='blackhole',
+            type='network',
+            description='Arista 7050SX Switch.',
+            children=[],
         ),
+        servers['overheat']._replace(type='raspi'),
+        servers['tornado']._replace(type='nuc'),
+    ])
 
-        Host.from_ldap(
-            hostname='jaws',
-            type='hypervisor',
-            children=[
-                Host.from_ldap(hostname)
-                for hostname in (
-                    'whirlwind',
-                )
-            ],
-        ),
-
-        Host.from_ldap(
-            hostname='pandemic',
-            type='hypervisor',
-            children=[
-                Host.from_ldap(hostname)
-                for hostname in (
-                    'pileup',
-                )
-            ],
-        ),
-
-        Host.from_ldap(
-            hostname='riptide',
-            type='hypervisor',
-            children=[
-                Host.from_ldap(hostname)
-                for hostname in (
-                    'anthrax',
-                    'biohazard',
-                    'death',
-                    'dementors',
-                    'democracy',
-                    'firestorm',
-                    'flood',
-                    'fraud',
-                    'gridlock',
-                    'lethe',
-                    'lightning',
-                    'maelstrom',
-                    'monsoon',
-                    'pestilence',
-                    'reaper',
-                    'segfault',
-                    'supernova',
-                    'thunder',
-                    'tsunami',
-                    'vampires',
-                    'whiteout',
-                    'zombies',
-                )
-            ],
-        ),
-
-        Host.from_ldap(
-            hostname='corruption',
-            type='server',
-        ),
-
-        Host.from_ldap(
-            hostname='dev-fallingrocks',
-            type='server',
-        ),
-
-        Host('blackhole', 'network', 'Managed Arista 7050SX-64 Switch.', []),
-
-        Host('logjam', 'printer', '', []),
-        Host('pagefault', 'printer', '', []),
-        Host('papercut', 'printer', '', []),
-        Host.from_ldap('overheat', type='raspi'),
-        Host.from_ldap('tornado', type='nuc'),
-
-        *(Host.from_ldap(desktop, type='desktop')
-          for desktop in sorted(list_desktops())),
-    ]
+    return sorted(servers_to_display)
 
 
 def servers(doc, request):
