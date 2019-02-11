6c7778f54f6a6baca3fdf1908b9a39c68f7b2aac
Daniel Kessler <dkessler@berkeley.edu>
2019-02-10 17:12:10 -0800

    Use new hours2 library

--- ocfweb/api/hours.py
+++ ocfweb/api/hours.py
@@ -1,149 +1,34 @@
-import operator
-from collections import namedtuple
-from datetime import date
-from datetime import datetime
-from datetime import timedelta
+from datetime import time
+from json import JSONEncoder
 
-import requests
 from django.http import JsonResponse
+from ocflib.lab.hours2 import Hour
+from ocflib.lab.hours2 import HoursListing
+from ocflib.lab.hours2 import read_hours_listing
 
 from ocfweb.caching import periodic
 
-HOURS_SPREADSHEET = 'https://docs.google.com/spreadsheet/ccc?key=1FHgW0wCnAh49bZIw54eoBT9xEoJ3AGG7Y2LhQdi0Lvs&output=csv'  # noqa: E501
-SHIFT_LENGTH = timedelta(hours=1)
 
-
-class Hour(namedtuple('Hours', ['open', 'close', 'staffer'])):
-
-    def __contains__(self, when):
-        if isinstance(when, datetime):
-            when = when.time()
-
-        return self.open <= when < self.close
-
-
-def _get_hours():
-    """pull hours from the Google spreadsheet and parse shifts."""
-
-    response = requests.get(HOURS_SPREADSHEET)
-    response.raise_for_status()
-
-    matrix = response.text.splitlines()
-    matrix = [row.split(',') for row in matrix]
-
-    # row[0] = ['13:30PM', 'name1', 'name2', ...]
-    shifts = [row[0] for row in matrix]
-
-    # first row is a header, first col is times: ['', 'Monday', ...]
-    header = shifts.pop(0)
-    assert header == '', header
-
-    all_shifts = {}
-
-    # instead of e.g. enumerate(['Monday', 'Tuesday', ... 'Sunday'])
-    for day in range(7):
-        # matrix[sidx + 1][day + 1] = person on shift on that shift index
-        all_shifts[day] = {
-            shift: matrix[sidx + 1][day + 1] for sidx, shift in enumerate(shifts)
-        }
-
-    #  this will get cached downstream
-    return all_shifts
-
-
-def _merge_shifts(first, second, staffer=True):
-    return Hour(
-        open=min(first.open, second.open),
-        close=max(first.close, second.close),
-        staffer=first.staffer if staffer else None,
-    )
-
-
-def _combine_shifts(shifts):
-    """combine a list of shifts into a list of Hour()s.
-
-    >>> _combine_shifts({'16:00PM':'test1', '16:30PM':'test2', ...})
-    [Hour(open=time(16), close=time(17), staffer='test2'), ...]
-    """
-
-    raw_shifts = []
-    for shift, staffer in shifts.items():
-        if not staffer:  # skip unstaffed shifts
-            continue
-
-        open = datetime.strptime(shift, '%I:%M %p')  # 12-hour AM/PM i.e. 7:00PM
-        close = open + SHIFT_LENGTH
-        raw_shifts.append(Hour(open=open.time(), close=close.time(), staffer=staffer))
-
-    raw_shifts.sort(key=operator.attrgetter('open'))
-
-    combined_shifts = []
-
-    shift = raw_shifts.pop(0)
-    for next_shift in raw_shifts:
-        if (shift.close in next_shift or next_shift.close in shift) and \
-                shift.staffer == next_shift.staffer:
-            shift = _merge_shifts(shift, next_shift)
+class JSONHoursEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, HoursListing):
+            return obj.__dict__
+        elif isinstance(obj, Hour):
+            return [obj.open, obj.close]
+        elif isinstance(obj, time):
+            return obj.strftime('%H:%M:%S')
         else:
-            combined_shifts.append(shift)
-            shift = next_shift
-
-    # tail case where staffer condition doesn't trip on list end
-    combined_shifts.append(shift)
-
-    return combined_shifts
+            return JSONEncoder.default(self, obj)
 
 
-def _generate_regular_hours():
-    raw_hours = _get_hours()
-    return {day: _combine_shifts(shifts) for day, shifts in raw_hours.items()}
-
-
-@periodic(5 * 60)
-def display_hours():
-    """merge shifts even further for display.
-
-    ocflib previously provided a list of hours separated when there
-    were discontinuous shifts. This hours implementation separates
-    hours when different opstaff are on shift as well for future big
-    brother tracking purposes, but this breaks applications that depend on
-    a concise list of hours for display. This is a fix that presents hours
-    like the old implementation until the code can be refactored as part
-    of the overall big brother project, i.e., in ocflib.
-    """
-
-    regular_hours = _generate_regular_hours()
-
-    display_hours = {}
-
-    for day, hours in regular_hours.items():
-        merged_shifts = []
-
-        hour = hours.pop(0)
-        for next_hour in hours:
-            if hour.close in next_hour or next_hour.close in hour:
-                hour = _merge_shifts(hour, next_hour, False)
-            else:
-                merged_shifts.append(hour)
-                hour = next_hour
-
-        merged_shifts.append(hour)
-
-        display_hours[day] = merged_shifts
-
-    return display_hours
-
-
-def get_hours_all(request):
-    return JsonResponse(
-        display_hours(),
-        json_dumps_params={'default': lambda x: x.isoformat()},
-    )
+@periodic(60)
+def get_hours_listing():
+    return read_hours_listing()
 
 
 def get_hours_today(request):
     return JsonResponse(
-        display_hours()[date.today().weekday()],
-        json_dumps_params={'default': lambda x: x.isoformat()},
+        get_hours_listing().hours_on_date(),
+        encoder=JSONHoursEncoder,
         safe=False,
     )
--- ocfweb/context_processors.py
+++ ocfweb/context_processors.py
@@ -1,13 +1,12 @@
 import re
-from datetime import date
 from ipaddress import ip_address
 
 from django.urls import reverse
 from ipware import get_client_ip
 from ocflib.account.search import user_is_group
 from ocflib.infra.net import is_ocf_ip
-from ocflib.lab.hours import Day
 
+from ocfweb.api.hours import get_hours_listing
 from ocfweb.component.lab_status import get_lab_status
 from ocfweb.component.session import logged_in_user
 from ocfweb.environment import ocfweb_version
@@ -24,15 +23,16 @@ def get_base_css_classes(request):
 
 
 def ocf_template_processor(request):
-    hours = Day.from_date(date.today())
+    hours_listing = get_hours_listing()
     real_ip, _ = get_client_ip(request)
     user = logged_in_user(request)
     return {
         'base_css_classes': ' '.join(get_base_css_classes(request)),
-        'current_lab_hours': hours,
+        'current_lab_hours': hours_listing.hours_on_date(),
+        'holidays': hours_listing.holidays,
         'is_ocf_ip': is_ocf_ip(ip_address(real_ip)) if real_ip else True,
         'join_staff_url': request.build_absolute_uri(reverse('about-staff')),
-        'lab_is_open': hours.is_open(),
+        'lab_is_open': hours_listing.is_open(),
         'lab_status': get_lab_status(),
         'ocfweb_version': ocfweb_version(),
         'request_full_path': request.get_full_path(),
