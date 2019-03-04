2a91eb1d33afd978a9907a5ad958607358c822e3
Abizer Lokhandwala <1916822+abizer@users.noreply.github.com>
2019-03-04 07:54:19 -0800

    add functionality for bouncing shorturls (#473)

--- ocfweb/api/urls.py
+++ ocfweb/api/urls.py
@@ -3,9 +3,12 @@ from django.urls import path
 from ocfweb.api import hours
 from ocfweb.api import lab
 from ocfweb.api import session_tracking
+from ocfweb.api import shorturls
+
 
 urlpatterns = [
     path('hours/today', hours.get_hours_today, name='hours_today'),
     path('lab/desktops', lab.desktop_usage, name='desktop_usage'),
     path('session/log', session_tracking.log_session, name='log_session'),
+    path('shorturl/<slug:slug>', shorturls.bounce_shorturl, name='bounce_shorturl'),
 ]
