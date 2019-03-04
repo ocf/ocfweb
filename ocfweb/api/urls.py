ab3dfd4f09ce381dbcc75a242bdbf608fa7b5b34
Abizer Lokhandwala <abizer@ocf.berkeley.edu>
2019-03-04 08:42:54 -0800

    shorturls: change slug to path to match all allowed chars

--- ocfweb/api/urls.py
+++ ocfweb/api/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('hours/today', hours.get_hours_today, name='hours_today'),
     path('lab/desktops', lab.desktop_usage, name='desktop_usage'),
     path('session/log', session_tracking.log_session, name='log_session'),
-    path('shorturl/<slug:slug>', shorturls.bounce_shorturl, name='bounce_shorturl'),
+    path('shorturl/<path:slug>', shorturls.bounce_shorturl, name='bounce_shorturl'),
 ]
