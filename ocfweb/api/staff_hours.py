c1d5031809260e335b2a51ba2c0683d0cda49fb6
Bernard Zhao <bernardzhao@berkeley.edu>
2019-06-29 17:52:36 -0700

    Add Staff Hours to API (#520)
    * added staff hours to API
    
    * Requested changes

---
+++ ocfweb/api/staff_hours.py
@@ -0,0 +1,10 @@
+from django.http import JsonResponse
+
+from ocfweb.main.staff_hours import get_staff_hours as real_get_staff_hours
+
+
+def get_staff_hours(request):
+    return JsonResponse(
+        [item._asdict() for item in real_get_staff_hours()],
+        safe=False,
+    )
--- ocfweb/api/urls.py
+++ ocfweb/api/urls.py
@@ -4,9 +4,11 @@ from ocfweb.api import hours
 from ocfweb.api import lab
 from ocfweb.api import session_tracking
 from ocfweb.api import shorturls
+from ocfweb.api import staff_hours
 
 
 urlpatterns = [
+    path('hours/staff', staff_hours.get_staff_hours, name='staff_hours'),
     path('hours/today', hours.get_hours_today, name='hours_today'),
     path('lab/desktops', lab.desktop_usage, name='desktop_usage'),
     path('session/log', session_tracking.log_session, name='log_session'),
