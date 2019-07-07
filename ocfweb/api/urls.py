493ebed3ffae82759922f5d9c3b5df94843d5b16
Bernard Zhao <bernardzhao@ocf.berkeley.edu>
2019-07-07 00:09:09 -0700

    Adding blog endpoint

--- ocfweb/api/urls.py
+++ ocfweb/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+from ocfweb.api import announce
 from ocfweb.api import hours
 from ocfweb.api import lab
 from ocfweb.api import session_tracking
@@ -8,6 +9,7 @@ from ocfweb.api import staff_hours
 
 
 urlpatterns = [
+    path('announce/blog/<int:limit>', announce.get_blog_posts, name='blog_posts'),
     path('hours/staff', staff_hours.get_staff_hours, name='staff_hours'),
     path('hours/today', hours.get_hours_today, name='hours_today'),
     path('lab/desktops', lab.desktop_usage, name='desktop_usage'),
