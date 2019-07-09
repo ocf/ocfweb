40535ce6e9306ac2e41029cf22b8ac9f16e32608
Bernard Zhao <bernard.zhao.us@gmail.com>
2019-07-09 08:19:34 -0700

    Add Blog to API (#523)
    * Adding blog endpoint
    
    * Simplified endpoint

---
+++ ocfweb/api/announce.py
@@ -0,0 +1,10 @@
+from django.http import JsonResponse
+
+from ocfweb.component.blog import get_blog_posts as real_get_blog_posts
+
+
+def get_blog_posts(request):
+    return JsonResponse(
+        [item._asdict() for item in real_get_blog_posts()],
+        safe=False,
+    )
