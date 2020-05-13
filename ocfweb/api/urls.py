f27c99396822e8f0b25762fb9016c76c9e28a603
BernardZhao <bernard.zhao.us@gmail.com>
2020-05-13 02:09:47 -0700

    Added num users and staff to api

--- ocfweb/api/urls.py
+++ ocfweb/api/urls.py
@@ -6,6 +6,7 @@ from ocfweb.api import lab
 from ocfweb.api import session_tracking
 from ocfweb.api import shorturls
 from ocfweb.api import staff_hours
+from ocfweb.api import stats
 
 
 urlpatterns = [
@@ -15,4 +16,10 @@ urlpatterns = [
     path('lab/desktops', lab.desktop_usage, name='desktop_usage'),
     path('session/log', session_tracking.log_session, name='log_session'),
     path('shorturl/<path:slug>', shorturls.bounce_shorturl, name='bounce_shorturl'),
+    path('lab/num_users', stats.get_num_users_in_lab, name='get_num_users_in_lab'),
+    path('lab/staff', stats.get_staff_in_lab, name='get_staff_in_lab'),
+    # path('stats/printing/pages')
+    # path('stats/accounts')
+    # path('stats/mirrors')
+    # path('stats/sessions')
 ]
