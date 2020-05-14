2d422df2ff1abd05ec474d9603ce7232784f4c1a
BernardZhao <bernard.zhao.us@gmail.com>
2020-05-13 20:42:20 -0700

    Changed path

--- ocfweb/api/urls.py
+++ ocfweb/api/urls.py
@@ -14,8 +14,8 @@ urlpatterns = [
     path('hours/staff', staff_hours.get_staff_hours, name='staff_hours'),
     path('hours/today', hours.get_hours_today, name='hours_today'),
     path('lab/desktops', lab.desktop_usage, name='desktop_usage'),
+    path('lab/num_users', stats.get_num_users_in_lab, name='get_num_users_in_lab'),
+    path('lab/staff', stats.get_staff_in_lab, name='get_staff_in_lab'),
     path('session/log', session_tracking.log_session, name='log_session'),
     path('shorturl/<path:slug>', shorturls.bounce_shorturl, name='bounce_shorturl'),
-    path('stats/num_users', stats.get_num_users_in_lab, name='get_num_users_in_lab'),
-    path('stats/staff', stats.get_staff_in_lab, name='get_staff_in_lab'),
 ]
