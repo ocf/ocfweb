493a080c9707becbd44947f1eafafa9c3f5d27a8
Jonathan Zhang <jonathan@bbs4.us>
2021-11-08 13:48:06 -0800

    Accurate mirrors stats (#735)
    * Update session_stats.html
    
    * Update mirrors.py

--- ocfweb/stats/mirrors.py
+++ ocfweb/stats/mirrors.py
@@ -12,6 +12,7 @@ from ocflib.lab.stats import humanize_bytes
 from ocfweb.caching import periodic
 
 MIRRORS_EPOCH = date(2017, 1, 1)
+MIRRORS_REPORTING_FIXED = date(2021, 10, 11)
 
 
 def stats_mirrors(request: HttpRequest) -> HttpResponse:
@@ -28,15 +29,19 @@ def stats_mirrors(request: HttpRequest) -> HttpResponse:
             'semester_dists': semester_dists,
             'all_time_total': all_time_total,
             'all_time_dists': all_time_dists,
-            'start_date': current_semester_start(),
+            'start_date': current_semester_start() if (
+                current_semester_start() > MIRRORS_REPORTING_FIXED
+            ) else MIRRORS_REPORTING_FIXED,
         },
     )
 
 
 @periodic(86400)
 def bandwidth_semester() -> Tuple[Any, Any]:
-
-    data = bandwidth_by_dist(current_semester_start())
+    if current_semester_start() > MIRRORS_REPORTING_FIXED:
+        data = bandwidth_by_dist(current_semester_start())
+    else:
+        data = bandwidth_by_dist(MIRRORS_REPORTING_FIXED)
 
     total = humanize_bytes(sum(x[1] for x in data))
     by_dist = [(dist, humanize_bytes(bw)) for dist, bw in data]
