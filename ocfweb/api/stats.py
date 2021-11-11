d6ddf473cf415cdac21d9034f3843724b6de8193
bentref <bentref11@gmail.com>
2021-11-10 21:09:54 -0800

    Add get_printers_summary

--- ocfweb/api/stats.py
+++ ocfweb/api/stats.py
@@ -3,6 +3,7 @@ from django.http import JsonResponse
 
 from ocfweb.stats.summary import staff_in_lab
 from ocfweb.stats.summary import users_in_lab_count
+from ocfweb.stats.summary import printers
 
 
 def get_num_users_in_lab(request: HttpRequest) -> JsonResponse:
@@ -15,5 +16,11 @@ def get_num_users_in_lab(request: HttpRequest) -> JsonResponse:
 def get_staff_in_lab(request: HttpRequest) -> JsonResponse:
     return JsonResponse(
         staff_in_lab(),
+        safe=False,
+    )
+
+def get_printers_summary(request: HttpRequest) -> JsonResponse:
+    return JsonResponse(
+        printers(),
         safe=False,
     )
