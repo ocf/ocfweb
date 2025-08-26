ceec50c0156f41927cfb41a5ddbfbb98f69e1a94
sophiebsw <sophiebsw@gmail.com>
2025-08-25 19:10:58 -0700

    replace url with re_path

--- ocfweb/urls.py
+++ ocfweb/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import include
-from django.conf.urls import url
+from django.conf.urls import re_path
 from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.urls import reverse
@@ -27,64 +27,64 @@ from ocfweb.tv.urls import urlpatterns as tv
 
 urlpatterns = [
     # test pages
-    url(r'^test/status$', lambda _: HttpResponse('ok'), name='status'),
-    url(r'^test/session$', test_session, name='test_session'),
-    url(r'^test/periodic$', test_list_periodic_functions, name='test_list_periodic_functions'),
+    re_path(r'^test/status$', lambda _: HttpResponse('ok'), name='status'),
+    re_path(r'^test/session$', test_session, name='test_session'),
+    re_path(r'^test/periodic$', test_list_periodic_functions, name='test_list_periodic_functions'),
 
     # prometheus metrics
-    url('', include('django_prometheus.urls')),
+    re_path('', include('django_prometheus.urls')),
 
-    url(r'^$', home, name='home'),
-    url(r'^robots\.txt$', robots_dot_txt, name='robots.txt'),
-    url(r'^favicon.ico$', favicon, name='favicon'),
-    url(r'^.well-known/security\.txt$', security_dot_txt, name='security.txt'),
+    re_path(r'^$', home, name='home'),
+    re_path(r'^robots\.txt$', robots_dot_txt, name='robots.txt'),
+    re_path(r'^favicon.ico$', favicon, name='favicon'),
+    re_path(r'^.well-known/security\.txt$', security_dot_txt, name='security.txt'),
 
-    url(r'^staff-hours$', staff_hours, name='staff-hours'),
+    re_path(r'^staff-hours$', staff_hours, name='staff-hours'),
 
-    url(r'^account/', include(account)),
-    url(r'^announcements/', include(announcements)),
-    url(r'^docs/', include(docs)),
-    url(r'^login/', include(login)),
-    url(r'^stats/', include(stats)),
-    url(r'^lab_reservations/', include(lab_reservations)),
+    re_path(r'^account/', include(account)),
+    re_path(r'^announcements/', include(announcements)),
+    re_path(r'^docs/', include(docs)),
+    re_path(r'^login/', include(login)),
+    re_path(r'^stats/', include(stats)),
+    re_path(r'^lab_reservations/', include(lab_reservations)),
 
     # about pages
-    url(r'^about/staff$', about_staff, name='about-staff'),
-    url(r'^about/lab/open-source$', lab_open_source, name='lab-open-source'),
-    url(r'^about/lab/vote$', lab_vote, name='lab-vote'),
-    url(r'^about/lab/survey$', lab_survey, name='lab-survey'),
+    re_path(r'^about/staff$', about_staff, name='about-staff'),
+    re_path(r'^about/lab/open-source$', lab_open_source, name='lab-open-source'),
+    re_path(r'^about/lab/vote$', lab_vote, name='lab-vote'),
+    re_path(r'^about/lab/survey$', lab_survey, name='lab-survey'),
 
     # tv endpoints
-    url(r'^tv/', include(tv)),
+    re_path(r'^tv/', include(tv)),
 
     # API endpoints
-    url(r'^api/', include(api)),
+    re_path(r'^api/', include(api)),
 
     # hosting logos
-    url(
+    re_path(
         r'^images/hosted-logos/(?:index\.shtml)?$',
         lambda _: redirect(reverse('doc', args=('services/vhost/badges',)), permanent=True),
     ),
-    url(r'^images/hosted-logos/(.*)$', lambda _, image: redirect('hosting-logo', image, permanent=True)),
-    url(r'^hosting-logos/(.*)$', hosting_logo, name='hosting-logo'),
+    re_path(r'^images/hosted-logos/(.*)$', lambda _, image: redirect('hosting-logo', image, permanent=True)),
+    re_path(r'^hosting-logos/(.*)$', hosting_logo, name='hosting-logo'),
 
     # legacy redirects
-    url(r'^index\.s?html$', lambda _: redirect(reverse('home'), permanent=True)),
-    url(r'^staff_hours(?:\.cgi)?$', lambda _: redirect(reverse('staff-hours'), permanent=True)),
-    url(r'^staff-hours\.cgi$', lambda _: redirect(reverse('staff-hours'), permanent=True)),
-    url(r'^OCF/(?:index\.shtml)?$', lambda _: redirect(reverse('doc', args=('about',)), permanent=True)),
-    url(
+    re_path(r'^index\.s?html$', lambda _: redirect(reverse('home'), permanent=True)),
+    re_path(r'^staff_hours(?:\.cgi)?$', lambda _: redirect(reverse('staff-hours'), permanent=True)),
+    re_path(r'^staff-hours\.cgi$', lambda _: redirect(reverse('staff-hours'), permanent=True)),
+    re_path(r'^OCF/(?:index\.shtml)?$', lambda _: redirect(reverse('doc', args=('about',)), permanent=True)),
+    re_path(
         r'^OCF/(?:past_)?officers.shtml$',
         lambda _: redirect(reverse('doc', args=('about/officers',)), permanent=True),
     ),
-    url(r'^OCF/staff/(?:index\.shtml)?$', lambda _: redirect(reverse('doc', args=('staff',)), permanent=True)),
-    url(
+    re_path(r'^OCF/staff/(?:index\.shtml)?$', lambda _: redirect(reverse('doc', args=('staff',)), permanent=True)),
+    re_path(
         r'^OCF/staff/where-now\.shtml$',
         lambda _: redirect(reverse('doc', args=('about/formerstaff',)), permanent=True),
     ),
-    url(r'^OCF/policies(?:/|$)', lambda _: redirect(reverse('docs'), permanent=True)),
-    url(r'^OCF/OCF_FAQ\.shtml$', lambda _: redirect(reverse('doc', args=('faq',)), permanent=True)),
-    url(r'^OCF/officers_.*\.s?html$', lambda _: redirect(reverse('doc', args=('about/officers',)), permanent=True)),
-    url(r'^OCF/staff/how-to-join\.shtml$', lambda _: redirect(reverse('about-staff'), permanent=True)),
-    url(r'^mlk$', lambda _: redirect(reverse('doc', args=('services/lab',)), permanent=True)),
+    re_path(r'^OCF/policies(?:/|$)', lambda _: redirect(reverse('docs'), permanent=True)),
+    re_path(r'^OCF/OCF_FAQ\.shtml$', lambda _: redirect(reverse('doc', args=('faq',)), permanent=True)),
+    re_path(r'^OCF/officers_.*\.s?html$', lambda _: redirect(reverse('doc', args=('about/officers',)), permanent=True)),
+    re_path(r'^OCF/staff/how-to-join\.shtml$', lambda _: redirect(reverse('about-staff'), permanent=True)),
+    re_path(r'^mlk$', lambda _: redirect(reverse('doc', args=('services/lab',)), permanent=True)),
 ]
